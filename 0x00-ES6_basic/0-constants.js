ae99a779dca8f7d57af5b55fd7bcdf801e2a9c76
vanessaokumu <okumuvanessa@gmail.com>
2024-03-07 02:21:52 -0800

    const and let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,24 +1,14 @@
-// Original function
-function taskFirst() {
-  var a = "1";
-  let b = "2";
-}
-
 // Modified function
 function taskFirst() {
     const a = 1;
-    const b = 2;
-    const sum = a + b;
-    console.log(sum);
+    console.log(a);
 }
 
 taskFirst();
 
 function taskNext() {
     let a = 1;
-    let b = 2;
-    let sum = a + b;
-    console.log(sum);
+    console.log(a);
 }
 
 taskNext();
