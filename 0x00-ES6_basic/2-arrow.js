ce64b7032428c559dae3d577a13e984bdb8268db
vanessaokumu <okumuvanessa@gmail.com>
2024-03-07 02:37:26 -0800

    arrow functions

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,12 @@
+const getNeighborhoodsList = () => {
+  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const addNeighborhood = (newNeighborhood) => {
+    sanFranciscoNeighborhoods.push(newNeighborhood);
+    return sanFranciscoNeighborhoods;
+  };
+
+  return { sanFranciscoNeighborhoods, addNeighborhood };
+};
+
+export default getNeighborhoodsList;
